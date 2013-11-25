2d723e0d0ef663a1962438816d39f55aa36f072c
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 10:26:12 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/nco/version.mk
+++ src/nco/version.mk
@@ -1,3 +1,14 @@
-NAME    = nco_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 4.2.3
-RELEASE = 3
+NAME               = nco_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 4.2.3
+RELEASE            = 3
+PKGROOT            = /opt/nco/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = nco
+
+SOURCE_NAME        = nco
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
