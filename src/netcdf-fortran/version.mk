5fe06dde3d2e41e5438d377d5f0a6ccc7ac44610
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 10:51:54 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/netcdf-fortran/version.mk
+++ src/netcdf-fortran/version.mk
@@ -1,3 +1,14 @@
-NAME    = netcdf-fortran_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 4.4b5
-RELEASE = 3
+NAME               = netcdf-fortran_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 4.4b5
+RELEASE            = 3
+PKGROOT            = /opt/netcdf/4.3.0/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = netcdf-fortran
+
+SOURCE_NAME        = netcdf-fortran
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
