e324cd5d372d47a65408b5f7fde2f6372188e542
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 11:00:17 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/netcdf/version.mk
+++ src/netcdf/version.mk
@@ -1,3 +1,14 @@
-NAME    = netcdf_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 4.3.0
-RELEASE = 3
+NAME               = netcdf_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 4.3.0
+RELEASE            = 3
+PKGROOT            = /opt/netcdf/$(VERSION)/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = netcdf
+
+SOURCE_NAME        = netcdf
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
