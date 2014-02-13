4b9b290651f03c16a78d470a34f6786faab4b52d
Jim Hayes <jhayes@sdsc.edu>
2014-02-13 13:56:20 -0800

    Correct source name from netcdf_362 to netcdf (source tar ball doesn't incorporate the version number).

--- src/netcdf_362/version.mk
+++ src/netcdf_362/version.mk
@@ -5,7 +5,7 @@ PKGROOT            = /opt/netcdf/$(VERSION)/$(ROLLCOMPILER)
 
 SRC_SUBDIR         = netcdf_362
 
-SOURCE_NAME        = netcdf_362
+SOURCE_NAME        = netcdf
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tgz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
