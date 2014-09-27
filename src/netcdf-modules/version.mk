4c59ecf8bc078879cc93824300ec075d7a9e6dbf
Jim Hayes <jhayes@sdsc.edu>
2014-09-26 19:04:04 -0700

    Modulefile rpm build restructuring.
    Adopt/adapt Rocks approach to copying version from corresponding s/w package.
    Move all variant definitions into version.mk so that identical modulefile
    Makefile will work across rolls.

--- src/netcdf-modules/version.mk
+++ src/netcdf-modules/version.mk
@@ -3,8 +3,14 @@ ifndef ROLLCOMPILER
 endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
-NAME       = netcdf-modules_$(COMPILERNAME)
-VERSION    = 4.3.1.1
-RELEASE    = 1
+NAME        = netcdf-modules_$(COMPILERNAME)
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/applications/.$(COMPILERNAME)/netcdf
 
-RPM.EXTRAS = AutoReq:No
+VERSION_SRC = $(REDHAT.ROOT)/src/netcdf/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+VERSION_ADD = 3.6.2
+
+RPM.EXTRAS  = AutoReq:No
