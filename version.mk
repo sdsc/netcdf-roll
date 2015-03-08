c653ae1af1d89a9d88975bb12cf3b4544daf3bde
Jerry Greenberg <jpg@comet-devel.local>
2015-03-08 11:55:22 -0700

    added avx2 option

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = netcdf
-RELEASE  = 0
-COLOR    = orange
+ROLLNAME        = netcdf
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)
