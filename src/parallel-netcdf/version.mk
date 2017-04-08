f1bbef5ea5644ec2592d0369c4b2f8db4a5643c7
Jim Hayes <jhayes@sdsc.edu>
2017-04-07 18:20:13 -0700

    Get rid of characters in source version that were breaking build.
    Resolves APS-444.

--- src/parallel-netcdf/version.mk
+++ src/parallel-netcdf/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-parallel-netcdf_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 1.5.0
-RELEASE        = 3
+RELEASE        = 4
 PKGROOT        = /opt/netcdf/4.3.2/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = parallel-netcdf
